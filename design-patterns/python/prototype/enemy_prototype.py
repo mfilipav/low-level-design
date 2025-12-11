aec59e9a9beb600ad9bd81aa6300379fc19f11ad
mfilipav <mfilipav@gmail.com>
2025-12-11 15:56:01 +0100

    feat: prototype

--- design-patterns/python/prototype/enemy_prototype.py
+++ design-patterns/python/prototype/enemy_prototype.py
@@ -1,10 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Any
 
 
 class EnemyPrototype(ABC):
     """Prototype interface for enemy cloning"""
-    
+
     @abstractmethod
-    def clone(self) -> 'EnemyPrototype':
+    def clone(self) -> "EnemyPrototype":
         pass
